e78bfcd4b8c23612845dc1ebfa2aa7db9a2f5d6b
Sumit05092002 <129786205+Sumit05092002@users.noreply.github.com>
2023-12-03 21:59:36 +0530

    2 questions solved

---
+++ squareRoot.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class squareRoot{
+
+    public static void main(String args[]){
+
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("Enter the element whose square root you want to calculate");
+        int element=Sc.nextInt();
+        int square=sqrt(element);
+    }
+
+    public static int sqrt(int element){
+
+
+    }
+}
